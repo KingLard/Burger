08a7f3d7e169470a0dfcf7dc22e1e19e88fa8882
Brett Landes <brettl0407@gmail.com>
2020-05-07 13:52:43 -0400

    added server.js file and sql files with information to create database

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Burger 1", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Burger 2", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Burger 3", false);
